3e5bbee329173f121d44f461ebc11ad974cd2fd4
qfisch <quentin.fisch@sensirion.com>
2025-12-23 11:31:24 +0100

    Added FullMeasurementFormatter

--- src/MeasurementFormatting.hpp
+++ src/MeasurementFormatting.hpp
@@ -19,6 +19,28 @@ namespace sensirion::upt::mqtt
         }
     };
 
+    struct FullMeasurementFormatter
+    {
+        std::string operator () (const sensirion::upt::core::Measurement& m){
+            std::stringstream stream{};
+            stream.precision(4);
+            stream << "{\"time_offset_ms\":";
+            stream << m.dataPoint.t_offset;
+            stream << ", \"value\":";
+            stream << m.dataPoint.value;
+            stream << ", \"device_id\":";
+            stream << m.metaData.deviceID;
+            stream << ", \"device_type\":";
+            stream << m.metaData.deviceType;
+            stream << ", \"signal\":";
+            stream << quantityOf(m.signalType);
+            stream << ", \"signal_unit\":";
+            stream << unitOf(m.signalType);
+            stream << "}";
+            return stream.str();
+        }
+    };
+
     struct DefaultMeasurementToTopicSuffix{
         std::string operator() (const sensirion::upt::core::Measurement& m){
             
